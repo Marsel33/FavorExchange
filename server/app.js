9a2e28ed76ff7e5e69091efda20fcaaa0f9bc3cb
Marsel <marsel.ibatullin1993@gmail.com>
2022-02-04 15:51:40 +0300

    app

--- server/app.js
+++ server/app.js
@@ -1,52 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
-const PORT = process.env.PORT || 3000
-const index = require('./src/routes/index')
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
-const fileUpload = require('express-fileupload')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
-app.set('view engine', 'hbs')
-app.set('views', path.join(process.env.PWD, 'src', 'views'))
 
-app.use(express.static(path.join(process.env.PWD, 'public')))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(fileUpload())
+const index = require('./routes/index');
 
-const sessionConfig = {
-  store: new FileStore(),
-  key: 'musicCookie',
-  secret: process.env.SECRET_KEY,
-  resave: false,
-  saveUninitialized: false,
-  httpOnly: true,
-  cookie: { expires: 24 * 60 * 60e3 },
-}
+const PORT = 3001;
+const app = express();
 
-const sessionParser = session(sessionConfig)
-app.use(sessionParser)
 
-hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'))
-// hbs.registerHelper('checkAuthor', function (value, value2) {
-//   return value == value2
-// })
+app.use(express.json());
+app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  if (req.session.userId) {
-    res.locals.userId = req.session.userId
-    res.locals.userEmail = req.session.userEmail
-    res.locals.userName = req.session.userName
-  }
-  next()
-})
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+}));
 
-app.use('/', index)
+app.use(
+    session({
+      name:'sid',
+      store: new FileStore({}),
+      saveUninitialized: false,
+      secret: 'lalalalal',
+      resave: false,
+    })
+);
 
+
+app.use('/', index);
 
 app.listen(PORT, () => {
-  console.log(`Server has been started on PORT: ${PORT}`)
+    console.log('Server start on port ', PORT)
 })
