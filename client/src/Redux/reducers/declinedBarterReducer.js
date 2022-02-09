2fbff58986fd5c71d590bc1ec1326d63bd3328b6
Черногорцева Полина <cernogorcevapolina@MacBook-Pro-Cernogorceva.local>
2022-02-09 14:49:47 +0300

    delete

--- client/src/Redux/reducers/declinedBarterReducer.js
+++ client/src/Redux/reducers/declinedBarterReducer.js
@@ -1,10 +1,14 @@
 import initState from "../init/initState";
-import {GET_ALL_DECLINE_BARTERS} from "../types/types";
+import {DELETE_DECLINE_BARTERS, GET_ALL_DECLINE_BARTERS} from "../types/types";
 
 export const declinedBarterReducer = (state = initState, action) => {
     switch (action.type) {
         case GET_ALL_DECLINE_BARTERS:
             return action.payload.filter(e => e.status === 'declined')
+        case DELETE_DECLINE_BARTERS:
+            const temp = [...state]
+            console.log(action.payload)
+            return temp.filter(el => el.barterId !== Number(action.payload))
         default :
             return state
     }
