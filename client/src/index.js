58c39173d9f36694d3d4c1df559b027ac8de8810
Черногорцева Полина <cernogorcevapolina@MacBook-Pro-Cernogorceva.local>
2022-02-10 10:03:44 +0300

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,28 +4,27 @@ import 'antd/dist/antd.css'
 import './firebase';
 import App from './app/App';
 import 'antd/dist/antd.min.css';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
+import {Provider} from 'react-redux';
+import {BrowserRouter} from "react-router-dom";
 import axios from 'axios';
 
 import store from './Redux/store/store';
 
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://favour-exchange-project.herokuapp.com/api';
 axios.defaults.withCredentials = true;
 
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
 
-  document.getElementById('root')
+    document.getElementById('root')
 );
 
 
